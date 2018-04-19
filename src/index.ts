5ad5aee7022495ab1475191aa772aa93a80a404f
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-04-19 22:34:33 +0200

    :rocket: add sqip wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-export {}
+import sqip, { Primitive } from "sqip"
+import * as sizeOf from "image-size"
+
+export default function svgify(
+  filename: string,
+  numberOfPrimitives: number,
+  blur = 0,
+  mode = Primitive.Combo
+) {
+  const { width, height, type } = sizeOf(filename)
+  const options = {
+    mode,
+    blur,
+    numberOfPrimitives,
+    width,
+    height,
+    filename
+  }
+  const { final_svg, img_dimensions, svg_base64encoded } = sqip(options)
+  return {
+    width,
+    height,
+    type,
+    filename,
+    svg: final_svg,
+    base64: svg_base64encoded
+  }
+}
