0d24b50285a0345fc0a01ab73b12ce304572c814
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-04-20 14:11:45 +0200

    :memo: :art: add test results to Readme

--- src/tests/testCases.ts
+++ src/tests/testCases.ts
@@ -5,17 +5,29 @@ const file = "./norway.jpg"
 
 export default [
   // tslint:disable:no-magic-numbers
-  // Useful
-  { numberOfPrimitives: 10, blur: 10, mode: Primitive.Combo },
-  { numberOfPrimitives: 50, blur: 10, mode: Primitive.Combo },
-  { numberOfPrimitives: 100, blur: 10, mode: Primitive.Combo },
-  { numberOfPrimitives: 500, blur: 10, mode: Primitive.Combo },
-
   // No Blur
   { numberOfPrimitives: 10, blur: 0, mode: Primitive.Combo },
   { numberOfPrimitives: 50, blur: 0, mode: Primitive.Combo },
   { numberOfPrimitives: 100, blur: 0, mode: Primitive.Combo },
   { numberOfPrimitives: 500, blur: 0, mode: Primitive.Combo },
+
+  // 10 Blur
+  { numberOfPrimitives: 10, blur: 10, mode: Primitive.Combo },
+  { numberOfPrimitives: 50, blur: 10, mode: Primitive.Combo },
+  { numberOfPrimitives: 100, blur: 10, mode: Primitive.Combo },
+  { numberOfPrimitives: 500, blur: 10, mode: Primitive.Combo },
+
+  // 25 Blur
+  { numberOfPrimitives: 10, blur: 25, mode: Primitive.Combo },
+  { numberOfPrimitives: 50, blur: 25, mode: Primitive.Combo },
+  { numberOfPrimitives: 100, blur: 25, mode: Primitive.Combo },
+  { numberOfPrimitives: 500, blur: 25, mode: Primitive.Combo },
+
+  // 100 Blur
+  { numberOfPrimitives: 10, blur: 100, mode: Primitive.Combo },
+  { numberOfPrimitives: 50, blur: 100, mode: Primitive.Combo },
+  { numberOfPrimitives: 100, blur: 100, mode: Primitive.Combo },
+  { numberOfPrimitives: 500, blur: 100, mode: Primitive.Combo },
 
   // Only Triangles
   { numberOfPrimitives: 10, blur: 10, mode: Primitive.Triangle },
