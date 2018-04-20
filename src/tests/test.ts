2463aa7052b3e809cb8749e8cdbfe46c5811d748
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-04-20 12:24:21 +0200

    :white_check_mark: add test on reference image

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,1 +1,30 @@
-import * as module from ".."
+import m from ".."
+import { writeFileSync } from "fs"
+import { resolve, dirname, join } from "path"
+
+const file = "./norway.jpg"
+
+const enum Primitive {
+  Combo = 0,
+  Triangle = 1,
+  Rect = 2,
+  Ellipse = 3,
+  Circle = 4,
+  RotatedRect = 5,
+  Beziers = 6,
+  RotatedEllipse = 7,
+  Polygon = 8
+}
+
+// tslint:disable-next-line:no-magic-numbers
+const result = m(file, 20, 12, Primitive.Combo)
+const base = join(dirname(resolve(file)), "src", "tests", "result.")
+
+writeFileSync(base + "svg", result.svg)
+writeFileSync(base + "base64.txt", result.base64)
+
+console.log(`
+  Testing svgify on: ${file}
+    Type: ${result.type}, Dimensions: ${result.width}x${result.height}
+    Results are placed in "${base}*
+`)
